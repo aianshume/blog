da26867d39c2f7bf88f677646322982e028d0618
Devin Schulz <devin@devinschulz.com>
2022-04-20 15:25:20 -0400

    Initial v10 commit

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+import preact from '@astrojs/preact'
+import tailwind from '@astrojs/tailwind'
+import { defineConfig } from 'astro/config'
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [preact(), tailwind()],
+  server: {
+    port: 1337,
+  },
+})
